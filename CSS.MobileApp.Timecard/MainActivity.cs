b4c0a0ac4ab74ab3305d333a4f20bfc0e8375de7
m_ono <m-ono@chubu-ishikai.or.jp>
2017-06-29 16:44:04 +0900

    Spiner Test
    ユーザーリストテスト

--- CSS.MobileApp.Timecard/MainActivity.cs
+++ CSS.MobileApp.Timecard/MainActivity.cs
@@ -1,18 +1,57 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content.PM;
+using System;
+using CSS.Library.Timecard.DAO;
+using CSS.Library.Timecard.Entity;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CSS.MobileApp.Timecard
 {
-    [Activity(Label = "CSS.MobileApp.Timecard", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "勤怠システム", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : Activity
     {
+        Button timeStamp;
+        Spinner SpinnerUserLists;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
+
+            SpinnerUserLists = FindViewById<Spinner>(Resource.Id.spinnerUserLists);
+
+            List<UserList.User> Users = new List<UserList.User>();
+
+            Config.Properties props = new Config.Properties();
+            props.User = "administrator";
+            props.Password = "chubu#201";
+            props.UriAdress = "192.168.250.200";
+            props.FolderName = "/share/test/";
+
+            CsvToList csvList = new CsvToList();
+
+            SharedFile smbUserList = new SharedFile(props);
+            Users = csvList.Users(smbUserList.SmbReader("UserList.txt"));
+            ArrayAdapter adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, Users);
+
+            SpinnerUserLists.Adapter = adapter;
+
+            timeStamp = FindViewById<Button>(Resource.Id.buttonStamp);
+
+            timeStamp.Click += Stamp;
+
+        }
+
+        private void Stamp(object sender, EventArgs e)
+        {
+            DateTime nowDate = DateTime.Now;
+
+
         }
     }
 }
--- CSS.MobileApp.Timecard/Resources/Resource.Designer.cs
+++ CSS.MobileApp.Timecard/Resources/Resource.Designer.cs
@@ -60,8 +60,11 @@ namespace CSS.MobileApp.Timecard
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int calendarView1 = 2131034114;
+			// aapt resource value: 0x7f050007
+			public const int buttonStamp = 2131034119;
+			
+			// aapt resource value: 0x7f050003
+			public const int calendarView1 = 2131034115;
 			
 			// aapt resource value: 0x7f050000
 			public const int linearLayout1 = 2131034112;
@@ -69,8 +72,17 @@ namespace CSS.MobileApp.Timecard
 			// aapt resource value: 0x7f050001
 			public const int linearLayout2 = 2131034113;
 			
-			// aapt resource value: 0x7f050003
-			public const int textClock1 = 2131034115;
+			// aapt resource value: 0x7f050004
+			public const int linearLayout3 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int spinnerUserLists = 2131034117;
+			
+			// aapt resource value: 0x7f050002
+			public const int textClock1 = 2131034114;
+			
+			// aapt resource value: 0x7f050006
+			public const int toggleAttendance = 2131034118;
 			
 			static Id()
 			{
